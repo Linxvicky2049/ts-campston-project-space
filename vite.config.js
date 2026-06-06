41f29b8e8f0c4bc3dfdf9a734fc4cbb90037b841
BLACKSUN_888 <51102797+Linxvicky2049@users.noreply.github.com>
2026-06-06 18:38:58 +0100

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: '/ts-campston-project-space/',  // Add this line
   plugins: [react()],
   server: {
     proxy: {

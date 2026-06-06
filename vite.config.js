fc8125e917ba6bf67ae555c1a4237ce6ee38b5eb
Linxvicky2049 <victor4all2015@gmail.com>
2026-06-06 15:50:18 +0100

    date 2026-06-15 14:30:00

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.le-systeme-solaire.net",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "")
-      }
-    }
-  }
+  base: '/ts-CAMPSTON-PROJECT-SPACE-/',
 })
